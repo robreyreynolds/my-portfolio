e246b474b3a244758e716dafe1500b55347a6ae2
Robert Reynolds <robert.reynolds@reycomllc.com>
2019-03-25 19:59:53 -0400

    Added SNS messaging on Portfolio publishing or publuishing failure

--- README.md
+++ README.md
@@ -6,6 +6,22 @@ This is my professional portfolio. It uses AWS and ReactJS.
 Brew
 Git and github
 SSH
-htmlCSS
-Font Awesome
-Google Fonts
+
+HTML
+CSS
+Font Awesome - For social media icons
+Google Fonts - For web site fonts
+
+iPython - for prototyping python scripts
+Python 2.7 - for Lambda script to deploy portfolio builds
+	Boto3 - for AWS commands within Python
+	StringIO - for in-memory file upload
+	Mimetypes - to add uploading mimetype guess from file extension
+	Zipfile - for uncompressing build artifact
+
+AWS Tools
+	S3
+	Lambda
+	CodeBuild
+	CloudFront
+	IAM
--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -6,18 +6,30 @@ import mimetypes
 
 def lambda_handler(event, context):
 
-    s3 = boto3.resource('s3')
-    portfolio_bucket = s3.Bucket('www.robertreynolds.net')
-    build_bucket = s3.Bucket('portfoliobuild.robertreynolds.net')
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:498771753159:deployPortfolioTopic')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj,nm,
-    			ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    try:
+        s3 = boto3.resource('s3')
+        portfolio_bucket = s3.Bucket('www.robertreynolds.net')
+        build_bucket = s3.Bucket('portfoliobuild.robertreynolds.net')
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj,nm,
+        			ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        print "Job done!"
+
+        topic.publish(Subject="Portfolio deployed", Message="Portfolio deployed successfully")
+
+    except:
+        topic.publish(Subject="Portfolio Deployment Failed", Message="The Portfolio was NOT deployed successfully")
+        raise
+
 
     return {
         'statusCode': 200,
