5c77c313af97efaac3355b3335ab7d127c9e60c0
Robert Reynolds <robert.reynolds@reycomllc.com>
2019-03-27 14:43:40 -0400

    Moved ExampleWork into a React component

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
-import 'react-dom'
-console.log("Loaded react-dom");
-console.log("Webpack works!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work.js';
+
+const myWork = [
+	{
+		'title': "Work Example",
+		'image': {
+			'descr': "example screenshot of a project involving code",
+			'src': "images/example1.png",
+			'comment': ""
+		}
+	},
+	{
+		'title': "Work Example",
+		'image': {
+			'descr': "example screenshot of a project involving chemistry",
+			'src': "images/example2.png",
+			'comment': `"Chemistry" by Surian Soosay is licensed under CC BY 2.0
+									https://www.flickr.com/photos/ssoosay/4097410999`
+		}
+	},
+	{
+		'title': "Work Example",
+		'image': {
+			'descr': "example screenshot of a project involving cats",
+			'src': "images/example3.png",
+			'comment': `"Bengal cat" by roberto shabs is licensed under CC BY 2.0
+									https://www.flickr.com/photos/37287295@N00/2540855181`
+		}
+	}
+]
+
+ReactDOM.render(<ExampleWork work={myWork} />, document.getElementById('example-work'));
